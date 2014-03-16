5b885a8a39410bbbeeea9c8c9025968045fca33d
Eka Putra <ekaputra@balitechy.com>
2014-03-16 14:06:29 +0800

    Edit requirements

--- lokerhub/settings.py
+++ lokerhub/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'lokerhub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.mysql',
+        'NAME': 'lokerhubdb',
+        'USER': 'lokerhubdbuser',
+        'PASSWORD': 'kvPzq2TeAqtR73Z',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
