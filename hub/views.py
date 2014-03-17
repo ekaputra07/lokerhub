8af131b0966756e59ad119d5ba25362137c1cb96
Eka Putra <ekaputra@balitechy.com>
2014-03-17 19:40:07 +0800

    Fix username

--- hub/views.py
+++ hub/views.py
@@ -83,11 +83,8 @@ def logincallback_view(request):
 
             u_firstname = profile.get('FirstName', '')
             u_lastname = profile.get('LastName', '')
-            if profile.get('ProfileName') and profile.get('ProfileName'):
-                u_username = profile.get('ProfileName',
-                                         'user%s' % random.randint(1000, 2000))
-            else:
-                u_username = 'user%s' % random.randint(1000, 2000))
+            u_username = profile.get('ProfileName',
+                                     'user%s' % random.randint(1000, 2000))
 
             try:
                 user = User.objects.get(email=u_email)
