5b8d82a7104ef79a1878b98d4d2aa9b728ffa29c
Eka Putra <ekaputra@balitechy.com>
2015-09-26 07:57:04 +0800

    Added cronjob script to remove duplicate indeed jobs

---
+++ remove-duplicate-jobs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mysql --user=lokerhubuser --password=lokerhubpassword2015! --database=lokerhubdb --execute="DELETE n1 FROM indeed_jobs n1, indeed_jobs n2 WHERE n1.id > n2.id AND n1.jobkey = n2.jobkey"
