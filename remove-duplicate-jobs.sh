533230274c9bab689ef32a7889b5b6564b851251
Eka Putra <ekaputra@balitechy.com>
2015-09-26 08:10:20 +0800

    Added duplicate indeed jobs remover script

--- remove-duplicate-jobs.sh
+++ remove-duplicate-jobs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo 'Remove duplicate Indeed Jobs'
 mysql --user=lokerhubuser --password=lokerhubpassword2015! --database=lokerhubdb --execute="DELETE n1 FROM indeed_jobs n1, indeed_jobs n2 WHERE n1.id > n2.id AND n1.jobkey = n2.jobkey"
